90a7e733b7c76c812865727b31b6d02d7c891970
novokrest <novokrest013@gmail.com>
2017-01-20 22:12:13 +0300

    use OWIN instead of Sytem.Web.WebApi.WebHost

--- CashExchangeMachine.WebApi.SelfHost/Program.cs
+++ CashExchangeMachine.WebApi.SelfHost/Program.cs
@@ -1,12 +1,21 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using Microsoft.Owin.Hosting;
 
 namespace CashExchangeMachine.WebApi.SelfHost
 {
     internal static class Program
     {
+        private static readonly string ServerUrl = ConfigurationManager.AppSettings["server"];
+
         public static void Main(string[] args)
         {
-            Server.Run(ConfigurationManager.AppSettings["server"]);
+            using (WebApp.Start<Startup>(url: ServerUrl))
+            {
+                Console.WriteLine("Server running at {0} - press Enter to quit. ", ServerUrl);
+                Console.WriteLine("Press Enter to quit...");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ CashExchangeMachine.WebApi.SelfHost/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using System.Web.Http;
+
+namespace CashExchangeMachine.WebApi.SelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration configuration = new HttpConfiguration();
+            WebApiConfig.Register(configuration);
+            appBuilder.UseWebApi(configuration);
+        }
+    }
+}
