825b8220112b32f464384a572d6d2fe9b1cf0533
novokrest <novokrest013@gmail.com>
2017-01-21 17:40:17 +0300

    [Storage][Tests] add tests for checking notes insertion

--- CashExchangeMachine.Core/Money/Notes.cs
+++ CashExchangeMachine.Core/Money/Notes.cs
@@ -1,13 +1,23 @@
 ﻿using CashExchangeMachine.Core.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CashExchangeMachine.Core.Money
 {
     public class Notes : RestrictedIntegerCollection
     {
-        public static Notes Create(Currency currency)
+        public static Notes Create(Currency currency, IEnumerable<Tuple<int, int>> noteNominalCountPairs = null)
         {
-            return new Notes(currency.NoteNominals);
+            noteNominalCountPairs = noteNominalCountPairs ?? Enumerable.Empty<Tuple<int, int>>();
+
+            var notes = new Notes(currency.NoteNominals);
+            foreach (var noteNominalCountPair in noteNominalCountPairs)
+            {
+                notes.Add(noteNominalCountPair.Item1, noteNominalCountPair.Item2);
+            }
+
+            return notes;
         }
 
         private Notes(IEnumerable<int> nominals)
---
+++ CashExchangeMachine.Storage.IntegrationTests/NoteRepositoryTests.cs
@@ -0,0 +1,74 @@
+﻿using CashExchangeMachine.Core.Money;
+using CashExchangeMachine.Storage.Sql;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashExchangeMachine.Storage.IntegrationTests
+{
+    [TestFixture]
+    internal class NoteRepositoryTests
+    {
+        private const string ConnectionString = @"Data Source=.\SQL2012EXPRESS;Initial Catalog=TestCashDB; Integrated Security=True";
+        private const string NoteTableName = "Notes";
+        private const string Currency = "Dollar";
+
+        private static readonly NoteEntity[] InitNoteEntities = new[] 
+        {
+            new NoteEntity { Nominal = 1, Currency = "Dollar", Count = 123 },
+            new NoteEntity { Nominal = 2, Currency = "Dollar", Count = 342 },
+            new NoteEntity { Nominal = 5, Currency = "Dollar", Count = 9574 },
+            new NoteEntity { Nominal = 10, Currency = "Dollar", Count = 332 }
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            TestTableInitializer.Create(ConnectionString, NoteTableName)
+                                .Initialize(InitNoteEntities);
+        }
+
+        [Test]
+        public void CheckAllNotesLoadedSuccessfullyWithoutCheckingNoteContents()
+        {
+            var noteRepository = GetNoteRepository();
+
+            var loadedNotes = noteRepository.Load(CurrencyRegistry.GetCurrency(Currency));
+
+            Assert.IsTrue(InitNoteEntities.Length == loadedNotes.Count);
+        }
+
+        [Test]
+        public void Given_NewNotes_Should_AddNotesSuccessfuly()
+        {
+            var noteRepository = GetNoteRepository();
+            // TODO: may be pass just Currency name ?
+            var notesBeforeInsertion = LoadNotes(noteRepository);
+
+            var note = new NoteEntity { Nominal = 1, Currency = Currency, Count = 10 };
+            noteRepository.Insert(note);
+
+            var notesAfterInsertion = LoadNotes(noteRepository);
+
+            Assert.IsTrue(notesAfterInsertion[1].Count == notesBeforeInsertion[1].Count + 10);
+        }
+
+        private NoteRepository GetNoteRepository()
+        {
+            var sqlConnectionProvider = new SqlConnectionProvider(ConnectionString);
+            var noteRepository = new NoteRepository(sqlConnectionProvider);
+
+            return noteRepository;
+        }
+
+        private IDictionary<int, NoteEntity> LoadNotes(NoteRepository noteRepository)
+        {
+            return noteRepository.Load(CurrencyRegistry.GetCurrency(Currency))
+                                                       .ToDictionary(noteEntity => noteEntity.Nominal, 
+                                                                     noteEntity => noteEntity);
+        }
+    }
+}
