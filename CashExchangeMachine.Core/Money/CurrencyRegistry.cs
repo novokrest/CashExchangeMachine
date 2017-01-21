4bdb15922062198e2ed468603f2b03f8a00848ab
novokrest <novokrest013@gmail.com>
2017-01-21 16:40:35 +0300

    [Core] add CurrencyRegistry

---
+++ CashExchangeMachine.Core/Money/CurrencyRegistry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashExchangeMachine.Core.Money
+{
+    public class CurrencyRegistry
+    {
+        private static readonly CurrencyRegistry Registry = new CurrencyRegistry();
+
+        static CurrencyRegistry()
+        {
+            RegisterCurrency(Currency.Dollar);
+        }
+
+        public static void RegisterCurrency(Currency currency)
+        {
+            Registry.RegisterCurrency(currency.Name, currency);
+        }
+
+        public static Currency GetCurrency(string currencyName)
+        {
+            return Registry[currencyName];
+        }
+
+        private readonly IDictionary<string, Currency> _currencyByName = new Dictionary<string, Currency>();
+
+        private Currency this[string name] => _currencyByName[name];
+
+        private void RegisterCurrency(string name, Currency currency)
+        {
+            Verifiers.Verify(!_currencyByName.ContainsKey(currency.Name), "Currency {0} has been already registered");
+            _currencyByName.Add(currency.Name, currency);
+        }
+    }
+}
